b94537703bf38c4f764f6b62d214e2be494cefcb
Dudz <142178060+Odudz@users.noreply.github.com>
2025-10-02 11:08:09 -0300

    Add files via upload

--- estrutura_controle/script.js
+++ estrutura_controle/script.js
@@ -1,34 +1,34 @@
 const idadeInput = document.getElementById('idade');
-const verifyButton = document.getElementById('verifyButton');
+const verifyButton = document.getElementById('verificarButton'); 
 const resultado = document.getElementById('resultado');
 
 function verificarIdade(){
   resultado.classList.remove('visivel');
-  const idade = parseInt(idade.value);
+  const idade = parseInt(idadeInput.value);
   let menssagem = '';
 
-  if(isNan(idade) || idade < 0){
+  if(isNaN(idade) || idade < 0){
     menssagem = 'Digite uma idade válida.';
-  }else if(idade > 18){
+  } else if(idade < 18){
     menssagem = "Você é menor de idade";
-  }else if(idade < 60){
+  } else if(idade < 60){
     menssagem = "Você é adulto";
-  }else{
+  } else{
     menssagem = "Você é senhor de idade";
   }
 
   setTimeout(() => {
-    resultado.innerText = menssagem
+    resultado.innerText = menssagem;
     resultado.classList.add('visivel');
   }, 100);
 }
 
-verifyButton.addEventListener('click', idade);
+verifyButton.addEventListener('click', verificarIdade);
 idadeInput.addEventListener('keyup', (event) => {
   if(event.key === 'Enter') verificarIdade();
 });
 
-const canvas = document.getElementById('backgound-canvas');
+const canvas = document.getElementById('background-canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
@@ -45,7 +45,7 @@ window.addEventListener('mousemove', (event) =>{
   mouse.y = event.y;
 });
 
-let pariculasArray = [];
+let particulasArray = [];
 const numeroDeParticulas = 100;
 
 class Particula{
@@ -61,8 +61,72 @@ class Particula{
   desenhar(){
     ctx.beginPath();
     ctx.arc(this.x,this.y,this.tamanho, 0, Math.PI * 2,false);
-    ctx.fillStyle = '#007bff';
+    ctx.fillStyle = this.cor;
     ctx.fill();
   }
 
+  atualizar(){
+    if(this.x > canvas.width || this.x < 0){
+      this.direcaoX = -this.direcaoX;
+    }
+    if(this.y > canvas.height || this.y < 0){
+      this.direcaoY = -this.direcaoY;
+    }
+    this.x += this.direcaoX;
+    this.y += this.direcaoY;
+    this.desenhar();
+  }
 }
+
+function init(){
+  particulasArray = [];
+  for(let i = 0; i < numeroDeParticulas; i++){
+    let tamanho = Math.random() * 2 + 1;
+    let x = Math.random() * (innerWidth - tamanho * 2) + tamanho;
+    let y = Math.random() * (innerHeight - tamanho * 2) + tamanho;
+    let direcaoX = (Math.random() * 0.4) - 0.2;
+    let direcaoY = (Math.random() * 0.4) - 0.2;
+    let cor = '#000000ff';
+    particulasArray.push(new Particula(x,y,direcaoX,direcaoY,tamanho,cor));
+  }
+}
+
+function conectar(){
+  for(let a = 0; a < particulasArray.length; a++){
+    for(let b = a + 1; b < particulasArray.length; b++){
+      let distancia = ((particulasArray[a].x - particulasArray[b].x) ** 2) + ((particulasArray[a].y - particulasArray[b].y) ** 2);
+    
+      if(distancia < (canvas.width/7) * (canvas.height/7)){
+        ctx.strokeStyle = `rgba(0,0,0, ${1 - (distancia/20000)})`;
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(particulasArray[a].x, particulasArray[a].y);
+        ctx.lineTo(particulasArray[b].x, particulasArray[b].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+function animar(){
+  requestAnimationFrame(animar);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for(let i = 0; i < particulasArray.length; i++){
+    particulasArray[i].atualizar();
+  }
+  conectar();
+}
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+})
+
+window.addEventListener('mouseout', () =>{
+  mouse.x = undefined;
+  mouse.y = undefined;
+})
+
+init();
+animar();
